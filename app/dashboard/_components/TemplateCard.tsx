43c9e23e694881c486afb0e4869ebc8397d19f81
Nigee Hettige <nigeehettige20@gmail.com>
2024-07-10 01:43:44 +0530

    modification 2

--- app/dashboard/_components/TemplateCard.tsx
+++ app/dashboard/_components/TemplateCard.tsx
@@ -4,12 +4,13 @@ import Image from "next/image";
 import Link from "next/link";
 
 function TemplateCard(item: TEMPLATE) {
+  const iconSrc = item.icon || "/logo.svg";
   return (
     <Link href={"/dashboard/content/" + item?.slug}>
       <div className="p-5 shadow-md rounded-md border  bg-white flex flex-col gap-3 cursor-pointer hover:scale-105 trasition-all">
-        <Image src={item.icon} alt="icon" width={50} height={50} />
-        <h2 className="font-medium text-lg">{item.name}</h2>
-        <p className="text-gray-500 line-clamp-3">{item.desc}</p>
+        <Image src={iconSrc}  alt="icon" width={50} height={50} />
+        <h2 className="font-medium text-lg">{item?.name}</h2>
+        <p className="text-gray-500 line-clamp-3">{item?.desc}</p>
       </div>
     </Link>
   );
